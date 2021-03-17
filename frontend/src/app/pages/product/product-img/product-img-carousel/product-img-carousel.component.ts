01e09b1b7ec38b575b439f91b52cfe5d01628dbb
Podar Razvan <razvanpodar.pr@gmail.com>
2021-03-17 03:00:40 +0200

    product carousel fixed

--- frontend/src/app/pages/product/product-img/product-img-carousel/product-img-carousel.component.ts
+++ frontend/src/app/pages/product/product-img/product-img-carousel/product-img-carousel.component.ts
@@ -1,20 +1,56 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Slide } from 'src/app/pages/home/flesh-deals/home-flesh-deals-carousel/home-flesh-deals-carousel.component';
 
 @Component({
   selector: 'app-product-img-carousel',
   templateUrl: './product-img-carousel.component.html',
-  styleUrls: ['./product-img-carousel.component.scss']
+  styleUrls: ['./product-img-carousel.component.scss'],
 })
-export class ProductImgCarouselComponent {
-
+export class ProductImgCarouselComponent implements OnInit {
   @Input() images;
-  @Output() index = new EventEmitter<number>();
-  
-  selectedImgIndex = 0;
+  @Output() selectedImg = new EventEmitter<string>();
 
-  imgSelected(index) {
-    this.selectedImgIndex = index;
-    this.index.emit(index);
+  slides: Slide[][] = [];
+  singleSlide: any;
+  index = 0;
+
+  ngOnInit() {
+    const elements = 2;
+
+    let times = 0;
+    let slide: Slide[] = [];
+
+    for (let i = 0; i < this.images.length; i++) {
+      times++;
+      slide.push( this.images[i]);
+
+      if (times === elements) {
+        times = 0;
+        this.slides.push(slide);
+        slide = [];
+      }
+    }
+    const slideLength = slide.length;
+    if (slideLength != 0) {
+      for (let i = slideLength - 1; i < elements - 1; i++) {
+        slide.push(this.images[i]);
+      }
+      this.slides.push(slide);
+    }
+    this.singleSlide = this.slides[this.index];
   }
 
+  imgSelected(img) {
+    this.selectedImg.emit(img);
+  }
+
+  next() {
+    this.index == this.singleSlide.length - 1 ? (this.index = 0) : this.index++;
+    this.singleSlide = this.slides[this.index];
+  }
+
+  previous() {
+    this.index == 0 ? (this.index = this.singleSlide.length - 1) : this.index--;
+    this.singleSlide = this.slides[this.index];
+  }
 }
--- frontend/src/app/pages/product/product-img/product-img.component.ts
+++ frontend/src/app/pages/product/product-img/product-img.component.ts
@@ -8,6 +8,6 @@ import { Component, Input } from '@angular/core';
 export class ProductImgComponent {
 
   @Input() product;
-  mainImgIndex;
+  selectedImg;
 
 }
