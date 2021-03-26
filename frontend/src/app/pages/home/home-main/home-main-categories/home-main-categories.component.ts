286b4c5b6443b86119a724cd2dd0e2b65bc6f623
Podar Razvan <razvanpodar.pr@gmail.com>
2021-03-26 10:43:51 +0200

    home-main-categories show only 9 categories

--- frontend/src/app/pages/home/home-main/home-main-categories/home-main-categories.component.ts
+++ frontend/src/app/pages/home/home-main/home-main-categories/home-main-categories.component.ts
@@ -7,6 +7,18 @@ import { SharedDataService } from 'src/app/shared/services/shared-data.service';
   styleUrls: ['./home-main-categories.component.scss'],
 })
 export class HomeMainCategoriesComponent {
+  categories = [];
+  numberOfCategories = 9;
   constructor(public sharedDataService: SharedDataService) {
+    this.sharedDataService.layout$.subscribe((response) => {
+      const categories = response.categories;
+      while (this.categories.length < this.numberOfCategories) {
+        const category =
+          categories[Math.floor(Math.random() * categories.length)];
+        if (this.categories.indexOf(category) === -1) {
+          this.categories.push(category);
+        }
+      }
+    });
   }
 }
