f7bbde86dcd111b62830fe3d4b2ea8d2a8a0efe3
Podar Razvan <razvanpodar.pr@gmail.com>
2021-04-12 19:01:11 +0300

    loading spinner on shop by brand

--- frontend/src/app/pages/home/shop-by-brand/shop-by-brand.component.ts
+++ frontend/src/app/pages/home/shop-by-brand/shop-by-brand.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { Brand } from 'src/app/shared/interfaces/brand.interface';
 import { IProduct } from 'src/app/shared/interfaces/product.interface';
+import { LoadingService } from 'src/app/shared/loading/loading.service';
 import { SharedDataService } from 'src/app/shared/services/shared-data.service';
 import { ProductsService } from '../../admin/products/products.service';
 
 @Component({
   selector: 'app-shop-by-brand',
   templateUrl: './shop-by-brand.component.html',
-  styleUrls: ['./shop-by-brand.component.scss']
+  styleUrls: ['./shop-by-brand.component.scss'],
 })
 export class ShopByBrandComponent {
   brands: Brand[];
@@ -17,14 +18,18 @@ export class ShopByBrandComponent {
   selectedBrand: Brand;
   products: IProduct[];
 
-  constructor(private sharedDataService: SharedDataService, private productsService: ProductsService) {
-    this.sharedDataService.layout$.subscribe((response)=> {
-      if(response.brands.length>0){
+  constructor(
+    private sharedDataService: SharedDataService,
+    private productsService: ProductsService,
+    private loadingService: LoadingService
+  ) {
+    this.sharedDataService.layout$.subscribe((response) => {
+      if (response.brands.length > 0) {
         this.brands = response.brands;
         this.selectedBrand = this.brands[0];
         this.getProducts(this.selectedBrand.name);
       }
-    })
+    });
   }
 
   selected(index) {
@@ -33,8 +38,14 @@ export class ShopByBrandComponent {
   }
 
   getProducts(name) {
-    this.productsService.getPaginatedProductsByBrand(this.page, this.pageLimit, name).subscribe((response)=> {
-      this.products = response
-    })
+    const products = this.productsService.getPaginatedProductsByBrand(
+      this.page,
+      this.pageLimit,
+      name
+    );
+    const products$ = this.loadingService.showLoaderUntilCompleted(products);
+    products$.subscribe((response) => {
+      this.products = response;
+    });
   }
 }
